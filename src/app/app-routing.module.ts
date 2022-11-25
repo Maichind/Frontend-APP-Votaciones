20cf81a28f71c95e32c9e27dcd34a723e1ef8b6c
Mindiola <maikmj101@gmail.com>
2022-11-25 12:07:24 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,34 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
-import { AppLayoutComponent } from "./layout/app.layout.component";
+import { HomeComponent } from './pages/seguridad/home/home.component';
+import { MesasComponent } from './pages/resultados/mesas/mesas.component';
+import { PartidosComponent } from './pages/resultados/partidos/partidos.component';
+import { CandidatosComponent } from './pages/resultados/candidatos/candidatos.component';
+import { ReportesComponent } from './pages/resultados/reportes/reportes.component';
 
 @NgModule({
     imports: [
         RouterModule.forRoot([
             {
-                path: '', component: AppLayoutComponent,
+                path: '', component: HomeComponent,
                 children: [
-                    { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
                     { path: 'uikit', loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule) },
                     { path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
                     { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
                     { path: 'blocks', loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
-                    { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) }
+                    { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) },
                 ]
             },
             { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
             { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
+            { path: '', component: HomeComponent },
             { path: 'notfound', component: NotfoundComponent },
+            { path: 'home', component: HomeComponent },
+            { path: 'mesas', component: MesasComponent },
+            { path: 'partidos', component: PartidosComponent },
+            { path: 'candidatos', component: CandidatosComponent },
+            { path: 'reportes', component: ReportesComponent },
             { path: '**', redirectTo: '/notfound' },
         ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
     ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,61 @@
 import { NgModule } from '@angular/core';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
-import { ProductService } from './demo/service/product.service';
-import { CountryService } from './demo/service/country.service';
-import { CustomerService } from './demo/service/customer.service';
-import { EventService } from './demo/service/event.service';
-import { IconService } from './demo/service/icon.service';
-import { NodeService } from './demo/service/node.service';
-import { PhotoService } from './demo/service/photo.service';
+import {CheckboxModule} from 'primeng/checkbox';
+import {PasswordModule} from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import {OverlayPanelModule} from 'primeng/overlaypanel';
+import { HomeComponent } from './pages/seguridad/home/home.component';
+import { StyleClassModule } from 'primeng/styleclass';
+import { DividerModule } from 'primeng/divider';
+import { ChartModule } from 'primeng/chart';
+import { PanelModule } from 'primeng/panel';
+import { RouterModule } from '@angular/router';
+import {TableModule} from 'primeng/table';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from './interceptors/token.interceptor';
+import { ToolbarModule } from 'primeng/toolbar';
+import { DialogModule } from 'primeng/dialog';
+import { MesasComponent } from './pages/resultados/mesas/mesas.component';
+import { ToastModule } from 'primeng/toast';
+import { PartidosComponent } from './pages/resultados/partidos/partidos.component';
+import { CandidatosComponent } from './pages/resultados/candidatos/candidatos.component';
+import { ReportesComponent } from './pages/resultados/reportes/reportes.component';
 
 @NgModule({
     declarations: [
-        AppComponent, NotfoundComponent
+        AppComponent, NotfoundComponent, HomeComponent, MesasComponent, PartidosComponent, CandidatosComponent, ReportesComponent
     ],
     imports: [
         AppRoutingModule,
-        AppLayoutModule
+        AppLayoutModule,
+        CheckboxModule,
+        ButtonModule,
+        InputTextModule,
+        FormsModule,
+        PasswordModule,
+        OverlayPanelModule,
+        DividerModule,
+        StyleClassModule,
+        ChartModule,
+        PanelModule,
+        RouterModule,
+        TableModule,
+        HttpClientModule,
+        ToolbarModule,
+        DialogModule,
+        ToastModule
     ],
     providers: [
-        { provide: LocationStrategy, useClass: HashLocationStrategy },
-        CountryService, CustomerService, EventService, IconService, NodeService,
-        PhotoService, ProductService
+        { 
+            provide: HTTP_INTERCEPTORS,
+            useClass: TokenInterceptor,
+            multi: true 
+        }
     ],
     bootstrap: [AppComponent]
 })
